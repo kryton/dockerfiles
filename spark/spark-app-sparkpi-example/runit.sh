d31d46a24664fe39739c77da73f3bd178a3cf6ca
Ian Holsman <kryton@gmail.com>
2014-05-19 11:59:39 -0500

    demo example script now works properly (with a name)

--- spark/spark-app-sparkpi-example/runit.sh
+++ spark/spark-app-sparkpi-example/runit.sh
@@ -1,2 +1,5 @@
 PREFIX=${1-spark}
-docker run --dns $(../scripts/ipof ${PREFIX}_dns ) -h demoapp.${PREFIX}.spark  kryton/spark-app-sparkpi-example start ${PREFIX}
+
+docker run --dns $(../scripts/ipof.sh ${PREFIX}_dns ) -h demoapp.${PREFIX}.spark  --name ${PREFIX}_sparkpi_example kryton/spark-app-sparkpi-example start ${PREFIX}
+#IP_Worker=$(./ipof.sh ${PREFIX}_sparkpi_example )
+#SSH_FWDS="$SSH_FWDS -L4040:${IP_Worker}:4040"
